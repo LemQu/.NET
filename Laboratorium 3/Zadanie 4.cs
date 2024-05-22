8fe1adf2b2a6db8f141bc40e9ac3fb1ae2253c24
LemQu <37375302+LemQu@users.noreply.github.com>
2024-05-22 09:14:43 +0200

    Add files via upload

---
+++ Laboratorium 3/Zadanie 4.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string originalFile = "test.txt";
+        string encryptedFile = "test.enc";
+        string decryptedFile = "test.dec";
+
+        GenerateKeys(out RSAParameters publicKey, out RSAParameters privateKey);
+
+        EncryptFile(originalFile, encryptedFile, publicKey);
+        DecryptFile(encryptedFile, decryptedFile, privateKey);
+
+        Console.WriteLine("Plik został zaszyfrowany i odszyfrowany.");
+    }
+
+    static void GenerateKeys(out RSAParameters publicKey, out RSAParameters privateKey)
+    {
+        using (var rsa = new RSACryptoServiceProvider(2048))
+        {
+            publicKey = rsa.ExportParameters(false);
+            privateKey = rsa.ExportParameters(true);
+        }
+    }
+
+    static void EncryptFile(string inputFile, string outputFile, RSAParameters publicKey)
+    {
+        byte[] fileBytes = File.ReadAllBytes(inputFile);
+
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            rsa.ImportParameters(publicKey);
+
+            byte[] encryptedBytes = rsa.Encrypt(fileBytes, false);
+
+            File.WriteAllBytes(outputFile, encryptedBytes);
+        }
+    }
+
+    static void DecryptFile(string inputFile, string outputFile, RSAParameters privateKey)
+    {
+        byte[] encryptedBytes = File.ReadAllBytes(inputFile);
+
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            rsa.ImportParameters(privateKey);
+
+            byte[] decryptedBytes = rsa.Decrypt(encryptedBytes, false);
+
+            File.WriteAllBytes(outputFile, decryptedBytes);
+        }
+    }
+}
